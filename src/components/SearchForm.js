113a6a9ca7d1f71f09af9f1d3246fe2d496d5cd8
marinaspas <msmarinaspas@gmail.com>
2021-03-17 21:04:33 -0400

    added all the initial components to my app

---
+++ src/components/SearchForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchForm = () => {
+  return (
+    <div>
+      <p>search form here</p>
+    </div>
+  );
+};
+
+export default SearchForm;
---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SingleCocktail() {
+  return (
+    <div>
+      <p>single cocktail page</p>
+    </div>
+  );
+}
+
+export default SingleCocktail;
