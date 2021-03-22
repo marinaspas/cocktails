6e95445c2cd8826463a30d53beabc5100ee1f2bf
marinaspas <msmarinaspas@gmail.com>
2021-03-21 20:42:58 -0400

    added the search form functionality to the app

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
 
-function SingleCocktail() {
+export default function SingleCocktail() {
   return (
     <div>
       <p>single cocktail page</p>
     </div>
   );
 }
-
-export default SingleCocktail;
